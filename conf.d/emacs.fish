29088263cf5c4486e2670d7fc0e328d1ecbe684b
Marcel Bollmann <marcel@bollmann.me>
2023-03-19 13:31:38 +0100

    Remove --preset from keybindings

--- conf.d/emacs.fish
+++ conf.d/emacs.fish
@@ -2,8 +2,8 @@ if not status is-interactive && test "$CI" != true
     exit
 end
 
-bind --preset -k f4 magit
-bind --preset -k f5 ed
+bind -k f4 magit
+bind -k f5 ed
 
 function _emacs_uninstall --on-event emacs_uninstall
     functions --erase ed et ew magit __emacs_launch
