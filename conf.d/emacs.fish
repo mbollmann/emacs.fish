9cdc9fc33f00fa2d33938b6ac742703940d24116
Marcel Bollmann <marcel@bollmann.me>
2025-06-07 19:39:36 +0200

    Make key bindings compatible with fish 4.x

--- conf.d/emacs.fish
+++ conf.d/emacs.fish
@@ -2,8 +2,13 @@ if not status is-interactive && test "$CI" != true
     exit
 end
 
-bind -k f4 magit
-bind -k f5 ed
+if test (string split -r '.' $version)[1] -gt 3
+    bind f4 magit
+    bind f5 ed
+else
+    bind -k f4 magit
+    bind -k f5 ed
+end
 
 function _emacs_uninstall --on-event emacs_uninstall
     functions --erase ed et ew magit __emacs_launch
